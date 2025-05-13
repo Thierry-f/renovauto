4fdc739f4d0fcc4446687632b47585f7d5746802
Thierry Faraci <thierry.faraci13@gmail.comgit commit -m Premier>
2025-05-13 09:09:37 +0200

    Premier commit

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+/*
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			alignment: 'center',
+			detach: false
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo h1').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+				$(function () {
+					$(window).scroll(function () { //Fonction appelée quand on descend la page
+						if ($(this).scrollTop() > 100 ) {  // Quand on est à 100pixels du haut de page,
+							$('#scrollup').css('right','10px'); // Replace à 10pixels de la droite l'image
+						} else { 
+							$('#scrollup').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+						}
+					});
+				});
+
+document.getElementById("year").innerHTML = new Date().getFullYear();			
+
+})(jQuery);
+
